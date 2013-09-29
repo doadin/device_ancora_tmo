4a56b522e301609c53a55f4b227001cf6fda4c23
Doadin <tbkizle@gmail.com>
2013-09-29 16:39:50 -0400

    Bring Back the Old
    Needs Fix To Compile

--- libsensors/AkmSensor 2.h
+++ libsensors/AkmSensor 2.h
@@ -33,30 +33,33 @@ struct input_event;
 
 class AkmSensor : public SensorBase {
 public:
-            AkmSensor();
-    virtual ~AkmSensor();
+	AkmSensor();
+	virtual ~AkmSensor();
 
-    enum {
-        Accelerometer   = 0,
-        MagneticField   = 1,
-        Orientation     = 2,
-        numSensors
-    };
+	enum {
+		Accelerometer = 0,
+		MagneticField,
+		Orientation,
+		RotationVector,
+		numSensors
+	};
 
-    virtual int setDelay(int32_t handle, int64_t ns);
-    virtual int enable(int32_t handle, int enabled);
-    virtual int readEvents(sensors_event_t* data, int count);
-    void processEvent(int code, int value);
+	virtual int setDelay(int32_t handle, int64_t ns);
+	virtual int setEnable(int32_t handle, int enabled);
+	virtual int readEvents(sensors_event_t* data, int count);
+	void processEvent(int code, int value);
+	int setAccel(sensors_event_t* data);
 
 private:
-    int loadAKMLibrary();
-    int update_delay();
-    void *mLibAKM;
-    uint32_t mEnabled;
-    uint32_t mPendingMask;
-    InputEventCircularReader mInputReader;
-    sensors_event_t mPendingEvents[numSensors];
-    uint64_t mDelays[numSensors];
+	int mEnabled[numSensors];
+	int64_t mDelay[numSensors];
+	uint32_t mPendingMask;
+	InputEventCircularReader mInputReader;
+	sensors_event_t mPendingEvents[numSensors];
+	char input_sysfs_path[PATH_MAX];
+	int input_sysfs_path_len;
+
+	int handle2id(int32_t handle);
 };
 
 /*****************************************************************************/
