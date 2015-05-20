15ee465039026a9dbc6cfb41157145113fb28809
Călin Neamţu <nilac8991@gmail.com>
2015-05-20 18:22:15 +0200

    board: Changed kernel path
    Included also necessary dependencies

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,7 @@ BOARD_USES_LEGACY_MMAP := true
 # Kernel
 BOARD_KERNEL_BASE := 0x00400000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/samsung/msm7x30-common
+TARGET_KERNEL_SOURCE := kernel/samsung/msm7x30
 TARGET_KERNEL_CONFIG := ancora_defconfig
 
 # Wifi related defines
