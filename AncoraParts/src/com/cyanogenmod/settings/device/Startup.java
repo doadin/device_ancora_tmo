3a7c7dea88939ba6a6ece7e071c71838d29fb956
sirmordred <bambu_bambu@windowslive.com>
2015-08-18 22:30:49 +0300

    Revert "AncoraParts: Removed all the uncompatible features for ancora"
    This reverts commit df73f1acc92fe928c25a4f11126aed9239cb9b60.

--- AncoraParts/src/com/cyanogenmod/settings/device/Startup.java
+++ AncoraParts/src/com/cyanogenmod/settings/device/Startup.java
@@ -26,5 +26,7 @@ public class Startup extends BroadcastReceiver {
     public void onReceive(final Context context, final Intent bootintent) {
         GeneralFragmentActivity.restore(context);
         PerformanceFragmentActivity.restore(context);
+        TouchKeyFragmentActivity.restore(context);
+        BacklightNotificationFragmentActivity.restore(context);
     }
 }
