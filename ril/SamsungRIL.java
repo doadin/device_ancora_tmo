8d18cf60e48e2d39996f443f8169e0568b4da1dc
sirmordred <bambu_bambu@windowslive.com>
2016-05-22 21:42:27 +0300

    SamsungRIL: Cleanup dead codes

--- ril/SamsungRIL.java
+++ ril/SamsungRIL.java
@@ -143,11 +143,6 @@ public class SamsungRIL extends RIL implements CommandsInterface {
         if (error == 0 || p.dataAvail() > 0) {
             // either command succeeds or command fails but with data payload
             try {switch (rr.mRequest) {
-            /*
-            cat libs/telephony/ril_commands.h \
-            | egrep "^ *{RIL_" \
-            | sed -re 's/\{([^,]+),[^,]+,([^}]+).+/case \1: ret = \2(p); break;/'
-             */
             case RIL_REQUEST_GET_SIM_STATUS: ret =  responseIccCardStatus(p); break;
             case RIL_REQUEST_ENTER_SIM_PIN: ret =  responseInts(p); break;
             case RIL_REQUEST_ENTER_SIM_PUK: ret =  responseInts(p); break;
@@ -268,7 +263,6 @@ public class SamsungRIL extends RIL implements CommandsInterface {
             case RIL_REQUEST_DIAL_EMERGENCY: ret = responseVoid(p); break;
             default:
                 throw new RuntimeException("Unrecognized solicited response: " + rr.mRequest);
-                //break;
             }} catch (Throwable tr) {
                 // Exceptions here usually mean invalid RIL responses
 
@@ -393,12 +387,6 @@ public class SamsungRIL extends RIL implements CommandsInterface {
         response = p.readInt();
 
         switch(response) {
-        /*
-				cat libs/telephony/ril_unsol_commands.h \
-				| egrep "^ *{RIL_" \
-				| sed -re 's/\{([^,]+),[^,]+,([^}]+).+/case \1: \2(rr, p); break;/'
-         */
-
         case RIL_UNSOL_NITZ_TIME_RECEIVED: ret =  responseString(p); break;
         case RIL_UNSOL_SIGNAL_STRENGTH: ret = responseSignalStrength(p); break;
         case RIL_UNSOL_CDMA_INFO_REC: ret = responseCdmaInformationRecord(p); break;
@@ -406,7 +394,6 @@ public class SamsungRIL extends RIL implements CommandsInterface {
         case RIL_UNSOL_STK_PROACTIVE_COMMAND: ret = responseString(p); break;
 
         //fixing anoying Exceptions caused by the new Samsung states
-        //FIXME figure out what the states mean an what data is in the parcel
 
         case RIL_UNSOL_O2_HOME_ZONE_INFO: ret = responseVoid(p); break;
         case RIL_UNSOL_DEVICE_READY_NOTI: ret = responseVoid(p); break;
@@ -836,7 +823,7 @@ public class SamsungRIL extends RIL implements CommandsInterface {
     responseCdmaSubscription(Parcel p) {
         String response[] = (String[])responseStrings(p);
 
-        if (/* mIsSamsungCdma && */ response.length == 4) {
+        if (response.length == 4) {
             // PRL version is missing in subscription parcel, add it from properties.
             String prlVersion = SystemProperties.get("ril.prl_ver_1").split(":")[1];
             response          = new String[] {response[0], response[1], response[2],
