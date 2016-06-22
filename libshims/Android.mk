06aae17fa9ceed2ac0a19a952cdaad032c83b4e9
sirmordred <bambu_bambu@windowslive.com>
2016-06-22 04:33:31 +0300

    [EXP] libshims: shim vectorimpl symbols

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -11,12 +11,13 @@ LOCAL_SRC_FILES := \
      ui/GraphicBufferMapper.cpp \
      media_shim.cpp \
      MemoryHeapPmem.cpp \
-     MemoryBase.c
+     MemoryBase.c \
+     VectorImpl.cpp
 
 #     SharedBuffer.cpp \
-#     VectorImpl.cpp
 
-#LOCAL_C_INCLUDES += external/safe-iop/include
+
+LOCAL_C_INCLUDES += external/safe-iop/include
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync libmedia
 
