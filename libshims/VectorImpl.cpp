00399d4b2185f8290900c2cffa68b033484bab4a
sirmordred <bambu_bambu@windowslive.com>
2016-06-23 07:51:25 +0300

    libshims: vectorimpl: Fix header

--- libshims/VectorImpl.cpp
+++ libshims/VectorImpl.cpp
@@ -25,7 +25,7 @@
 
 #include <utils/Errors.h>
 #include <utils/SharedBuffer.h>
-#include <utils/VectorImpl.h>
+#include "VectorImpl.h"
 
 /*****************************************************************************/
 
